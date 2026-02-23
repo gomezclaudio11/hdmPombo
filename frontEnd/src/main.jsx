b0561c6cb005fcbc6305c120dadf35866786934f
gomezclaudio11 <claudiogomez23@gmail.com>
2026-02-23 11:22:47 -0300

    router dom

--- frontEnd/src/main.jsx
+++ frontEnd/src/main.jsx
@@ -2,15 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client'; // <-- Importar el cliente de ReactDOM
 import App from './App.jsx';
 
-// 1. Obtener el elemento contenedor de la página HTML (normalmente un div con id="root")
-const rootElement = document.getElementById('root');
-
-// 2. Crear la raíz de React 18
-const root = ReactDOM.createRoot(rootElement);
-
-// 3. Renderizar el componente principal (<App />) dentro de la raíz
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
