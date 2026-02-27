29df58506f63b3ace93265e035b4f37168854db6
gomezclaudio11 <claudiogomez23@gmail.com>
2026-02-27 10:06:07 -0300

    auth Controller

--- backEnd/controllers/authController.js
+++ backEnd/controllers/authController.js
@@ -3,15 +3,24 @@ const jwt = require ("jsonwebtoken");
 
 exports.register = async (req, res) => {
     try {
-        const { nombre, email, password, role } = req.body;
+        const { nombre, email, password, codigoInvitacion } = req.body;
 
         // 1. verifico si el usuario existe
         let user = await User.findOne({ email });
         if (user) {
             return res.status(400).json({ message: "El usuario ya existe"})
         }
+        
+        // 2. Lógica de asignación de Roles 
+        // Definimos el rol por defecto
+        let role = 'reader'; 
+        
+        // Si el código es correcto, le damos el rol de observador
+        if (codigoInvitacion === 'agenteObservador') {
+            role = 'observer';
+        }
 
-        //2. creo un nuevo usuario
+        //3. creo un nuevo usuario
         //al hacer .save(), se dispara el pre-save hook y encripta la password
         user = new User ({
             nombre,
@@ -23,7 +32,9 @@ exports.register = async (req, res) => {
         await user.save();
 
         res.status(200).json({
-            message: "Usuario creado con exito",
+            message: role === "observer"
+                ? "Usuario creado con exito como OBSERVADOR"
+                : "Usuario creado con exito",
             user: {
                 id: user._id,
                 nombre: user.nombre,
@@ -56,7 +67,7 @@ exports.login = async (req, res) => {
 
         //3. si todo esta ok, crear el token JWT
         const token = jwt.sign(
-            {id: user._id, role: user.role }, //lo que queremos que el token sepa
+            {id: user._id, nombre: user.nombre, role: user.role }, //lo que queremos que el token sepa
             process.env.JWT_SECRET,           //clave secreta definida en .env
             { expiresIn: "8h" }               //tiempo de validez
         );
