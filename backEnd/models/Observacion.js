d39ed96c93e35304b1da6202753a19d0e9cd99e2
gomezclaudio11 <claudiogomez23@gmail.com>
2026-03-13 09:41:04 -0300

    creacion de archivo constants

--- backEnd/models/Observacion.js
+++ backEnd/models/Observacion.js
@@ -3,13 +3,54 @@ const mongoose = require('mongoose');
 // El nombre de las propiedades debe coincidir con los encabezados de CSV/JSON
 const ObservacionSchema = new mongoose.Schema({
     // --- CAMPOS NUEVOS (App React) ---
-    fecha: { type: Date, default: Date.now },
-    observador: String,
-    sector: String,
-    turno: String,
-    profesional: String,
-    momento: String,
-    accion: String,
+    fecha: { 
+        type: Date, 
+        default: Date.now,
+        require: [true, "La fecha es obligatoria"]
+    },
+    observador: {
+        type: String,
+        require: [true, "El nombre del observador es obligatorio"],
+        trim: true //limpia espacios en blancos accidentales
+    },
+    sector: {
+        type: String,
+        require: [ true, "El sector es obligatorio" ],
+        enum: {
+            values: ['7mo piso', '6to piso', '5to piso', '4to piso', 'UTI ADULTO', 'GUARDIA'],
+            message: "{VALUE} no es un sector valido"
+        }
+    },
+    turno: {
+        type: String,
+        require: [true, "El turno es obligatorio"],
+        enum: ['Mañana', 'Tarde', 'Noche A', 'Noche B', 'SADOFE', 'SADOFE NOCHE']
+    },
+    profesional: {
+        type: String,
+        require:[true, "El rol profesional es obligatorio"],
+        trim: true
+    },
+    momento: {
+        type: String,
+        require: [true, "El momento de observacion es obligatorio"],
+        enum: [
+            'Antes de tocar al paciente',
+            'Antes de realizar técnica Aseptica',
+            'Despues de tocar fluidos',
+            'Despues de tocar al paciente',
+            'Despues de tocar entorno'
+        ]
+    },
+    accion: {
+        type: String,
+        require: [true, "La ccion realizada es obligatoria"],
+        enum: [
+            'Higiene con solución alcoholica',
+            'Ninguna',
+            'Agua y Jabón (solo en Guardia)'
+        ]
+    },
 
     // --- CAMPOS VIEJOS (Google Forms / CSV) ---
     // Los mantenemos exactamente igual para que los datos históricos sigan ahí
@@ -25,5 +66,12 @@ const ObservacionSchema = new mongoose.Schema({
 }, {
     timestamps: true //// Esto agrega 'createdAt' y 'updatedAt' automáticamente
 });
+
+// ÍNDICES ESTRATÉGICOS:
+// Acelera los filtros por fecha y sector (los más usados en el Dashboard)
+ObservacionSchema.index({ fecha: -1, sector: 1 });
+
+// Acelera los rankings de profesionales
+ObservacionSchema.index({ profesional: 1 });
 
 module.exports = mongoose.model('Observacion', ObservacionSchema, 'observacions')
