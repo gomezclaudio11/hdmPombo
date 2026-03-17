52c86702984c1ff55e1588c28e08047c77b505b5
gomezclaudio11 <claudiogomez23@gmail.com>
2026-03-17 09:47:49 -0300

    agregue cambios para android/ios

--- frontEnd/src/main.jsx
+++ frontEnd/src/main.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client'; // <-- Importar el cliente de ReactDOM
 import App from './App.jsx';
 import { FilterProvider } from './context/FilterContext.jsx';
+import { registerSW } from "virtual:pwa-register"
 
+registerSW({ immediate: true })
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <FilterProvider>
--- frontEnd/vite.config.js
+++ frontEnd/vite.config.js
@@ -1,8 +1,38 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/"
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-controlled.png'],
+      manifest: {
+        name: 'HDM Pombo - Control de Higiene',
+        short_name: 'HDM Pombo',
+        description: 'Sistema de Vigilancia de Higiene de Manos - Hospital Pombo',
+        theme_color: '#0056b3', // El azul médico que elegimos
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
 })
